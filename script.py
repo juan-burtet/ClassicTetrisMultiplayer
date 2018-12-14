3a8e19abd3a9168f8b0cec91888a02cddbbc7f52
FelipeGruend <fcgruendemann@inf.ufpel.edu.br>
2018-12-14 03:44:03 -0200

    script para fechar conexao

---
+++ script.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = '127.0.0.1'              # Endereco IP do Servidor
+PORT = 5000                     # Porta que o Servidor esta
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+orig = (HOST, PORT)
+
+tcp.bind(orig)
+tcp.listen(1)
+
+#while True:
+#    con, cliente = tcp.accept()
+#    thread.start_new_thread(conectado, tuple([con, cliente]))
+
+tcp.close()
